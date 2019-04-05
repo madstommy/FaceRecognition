e14e703675160250e875722178bff21dc3a1bc1f
Thomas Cheal <tommycheal@hotmail.com>
2019-04-05 09:29:50 -0400

    Yeehaw

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ class App extends Component {
 
   onButtonSubmit=()=>{
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3001/imageurl', { 
+    fetch('https://boiling-lake-55425.herokuapp.com/imageurl', { 
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify( {
@@ -119,7 +119,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => {
         if(response){
-          fetch('http://localhost:3001/image', { 
+          fetch('https://boiling-lake-55425.herokuapp.com/image', { 
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify( {
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register', { 
+        fetch('https://boiling-lake-55425.herokuapp.com/register', { 
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify( {
